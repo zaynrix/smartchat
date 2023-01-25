8bc71827289a3fa00f523e91820b9e20ae2b17b4
mac <zayns1914@gmail.com>
2023-01-25 11:31:26 +0200

    refactor

--- lib/features/splashScreen.dart
+++ lib/features/splashScreen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../resources/assets_manager.dart';
-//
 
+//
+//
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
